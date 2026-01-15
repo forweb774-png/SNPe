001100db883afb6dc05b8b929f9546fa7965d454
forweb774-png <forweb774@gmail.com>
2026-01-15 11:38:10 +0200

    Added all the resources and transferred the main files

---
+++ script.js
@@ -0,0 +1,7 @@
+function saveComments(event) {
+  let yourSelection = document.querySelector("#check-box");
+  alert("Hello")
+}
+//submittion to the k-drive comes here
+let button = document.querySelector("button");
+button.addEventListener("click", saveComments);
